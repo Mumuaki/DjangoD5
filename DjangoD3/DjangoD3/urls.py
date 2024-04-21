3756691c8de735b998d4af32af6d037de576bc41
Mumuaki <151684806+Mumuaki@users.noreply.github.com>
2024-04-21 18:55:22 +0200

    Add files via upload

--- DjangoD3/DjangoD3/urls.py
+++ DjangoD3/DjangoD3/urls.py
@@ -20,7 +20,6 @@ from django.urls import path, include
 urlpatterns = [
    path('admin/', admin.site.urls),
    path('pages/', include('django.contrib.flatpages.urls')),
-   path('admin/', admin.site.urls),
    # path('accounts/', include('django.contrib.auth.urls')),
    # path("accounts/", include("accounts.urls")),
    path('accounts/', include('allauth.urls')),
